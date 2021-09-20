a9334100666c1fb8da7477443e1ced7a23d84a46
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-20 16:19:12 -0700

    Created useState for person to be invited and interface for the types.

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
+interface PeopleState {
+  people: {
+    id: number;
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
 export default function Stage() {
+  const [people, setPeople] = useState<PeopleState["people"]>([]);
+
   return (
     <div>
       <h1>This is the stage, hello</h1>
     </div>
-  )
+  );
 }
