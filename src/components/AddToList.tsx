13a2d5ddca97579483349bae1d015e2d293ed62d
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-21 07:48:16 -0700

    Styled AddToList component: centered form, added padding to input, textarea, and button. Changed width of all items and changed heading to h2. Pushed each inputs away with margin.

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,11 +1,12 @@
 export const AddToList = () => {
   return (
     <div className="AddContainer">
-      <p>Add to List</p>
+      <h2>Add to The List</h2>
       <input type="text" placeholder="Name" className="AddInput" />
       <input type="text" placeholder="Age" className="AddInput" />
       <input type="text" placeholder="Image Url" className="AddInput" />
-      <textarea placeholder="Notes" className="AddInput" />
+      <textarea placeholder="Notes" className="AddInput" rows={6} />
+      <button>Add To The List</button>
     </div>
   );
 };
