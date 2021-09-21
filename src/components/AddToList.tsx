968b69f6b733e646f9d445e9c00c10a6a10e3c27
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-21 08:14:05 -0700

    Added input states in AddToList and package UUID to handle ids.

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
 export const AddToList = () => {
+  const [input, setInput] = useState({
+    id: uuid(),
+    name: "",
+    age: "",
+    img: "",
+  });
+
   return (
     <div className="AddContainer">
       <h2>Add to The List</h2>
